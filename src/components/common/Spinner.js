a4db8df49f07dca5fcc01c7bfed3b11085c278ef
Christopher Dennis <cdennis.aus@gmail.com>
2018-08-18 18:29:52 -0500

    Updated Spinner component

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -3,13 +3,6 @@ import React from 'react';
 
 const Spinner = () => {
   return (
-    // <div>
-    //   <img
-    //     src={spinner}
-    //     style={{width: '200px', margin: 'auto', display: 'block'}}
-    //     alt='Loading...'
-    //   />
-    // </div>
     <div className='spinner'>
       <div className='circle'></div>
     </div>
