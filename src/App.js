9e41ca88786a8b6986a04229927569a26c0c8fbe
Christopher Dennis <cdennis.aus@gmail.com>
2018-08-19 13:13:55 -0500

    Fixed page loading on bottom with ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from './components/home/Home';
 import Movie from './components/movie-detail/MovieDetail';
 import Search from './components/search-results/SearchResults';
 import Footer from './components/layout/Footer';
+import ScrollToTop from './components/layout/ScrollToTop';
 
 import './sass/main.css';
 
@@ -23,18 +24,20 @@ class App extends Component {
     return (
       <Provider store={store}>
         <Router basename={process.env.PUBLIC_URL}>
-          <div className='app'>
-            <Header />
-            <Switch>
-              <Route
-                exact path={'/webflix'}
-                render={()=><Home />}
-              />
-              <Route exact path={'/movie'} component={Movie} />
-              <Route exact path={'/search'} component={Search} />
-            </Switch>
-            <Footer />
-          </div>
+          <ScrollToTop>
+            <div className='app'>
+              <Header />
+              <Switch>
+                <Route
+                  exact path={'/webflix'}
+                  render={()=><Home />}
+                />
+                <Route exact path={'/movie'} component={Movie} />
+                <Route exact path={'/search'} component={Search} />
+              </Switch>
+              <Footer />
+            </div>
+          </ScrollToTop>
         </Router>
       </Provider>
     );
