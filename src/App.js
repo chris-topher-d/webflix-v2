f017c2ea6bc5193310e5a65b2bb2ea4067185b1d
Christopher Dennis <cdennis.aus@gmail.com>
2018-09-28 23:29:45 -0500

    Added redirect for unknown urls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import store from './store/store';
 
 import Header from './components/layout/Header';
@@ -34,6 +34,7 @@ class App extends Component {
                 />
                 <Route exact path={'/movie'} component={Movie} />
                 <Route exact path={'/search'} component={Search} />
+                <Redirect to='/' />
               </Switch>
               <Footer />
             </div>
